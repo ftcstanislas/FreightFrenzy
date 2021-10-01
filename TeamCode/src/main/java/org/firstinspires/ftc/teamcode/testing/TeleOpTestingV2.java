f95ffa71eba5e4d978758d40cc60c1e6ba4a36f6
FTC Stanislas <ftcscw@gmail.com>
2021-10-01 10:50:01 +0200

    Update TeleOpTestingV2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TeleOpTestingV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TeleOpTestingV2.java
@@ -50,7 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+@TeleOp(name="Testing OpMode 2.0", group="Iterative Opmode")
 @Disabled
 public class TeleOpTestingV2 extends OpMode
 {
