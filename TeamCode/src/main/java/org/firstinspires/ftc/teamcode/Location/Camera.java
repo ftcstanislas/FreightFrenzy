321646ef5f56a708ddd49e4f797fcf0d50f01aa9
FTC Stanislas <ftcscw@gmail.com>
2021-11-10 12:56:39 +0100

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
@@ -245,7 +245,7 @@ public class Camera{
             // express position (translation) of robot in inches.
             VectorF translation = lastLocation.getTranslation();
             text += String.format("Pos (mm) {X, Y, Z} = %.1f, %.1f, %.1f",
-                    translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+                    translation.get(0) ,translation.get(1) ,translation.get(2));
 
             // express the rotation of the robot in degrees.
             Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
