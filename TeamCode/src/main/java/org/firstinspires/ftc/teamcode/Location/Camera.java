2b1010aa94fa66fb970bd02f3e4047c72e99b2c3
ftcstanislas <91455801+ftcstanislas@users.noreply.github.com>
2021-11-11 14:32:28 +0100

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
@@ -44,7 +44,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -93,6 +92,7 @@ public class Camera{
     // Class Members
     private OpenGLMatrix lastLocation   = null;
     private VuforiaLocalizer vuforia    = null;
+    private TFObjectDetector tfod       = null;
     private VuforiaTrackables targets   = null ;
     private WebcamName webcamName       = null;
 
@@ -100,7 +100,6 @@ public class Camera{
 
     // Telemetry
     protected Telemetry.Item telemetry = null;
-    protected Telemetry.Item telemetryDucks = null;
 
     //Trackelbles
     List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
@@ -111,10 +110,9 @@ public class Camera{
     // Pointer
     Servo pointer = null;
 
-    public void init(HardwareMap hardwareMap, Telemetry.Item telemetryInit,Telemetry.Item telemetryDucksInit) {
+    public void init(HardwareMap hardwareMap, Telemetry.Item telemetryInit) {
         // Telemetry
         telemetry = telemetryInit;
-        telemetryDucks = telemetryDucksInit;
 
         // Connect to the camera we are to use.  This name must match what is set up in Robot Configuration
         webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
@@ -208,6 +206,16 @@ public class Camera{
 //        }
 
         targets.activate();
+
+        //Duck
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+            "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
     }
 
     public void setCameraPosition(float forward, float left, float height, float heading){
@@ -243,7 +251,7 @@ public class Camera{
         }
 
         // Provide feedback as to where the robot is located (if we know).
-        String text = "";
+        text = "";
         if (targetVisible) {
             // express position (translation) of robot in inches.
             VectorF translation = lastLocation.getTranslation();
