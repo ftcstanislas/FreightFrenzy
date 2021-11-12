8868bcf9ca27a55cd841d46e6c912e806567a369
ftcstanislas <91455801+ftcstanislas@users.noreply.github.com>
2021-11-12 10:31:40 +0100

    changes to init camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
@@ -230,6 +230,18 @@ public class Camera{
         tfodParameters.inputSize = 320;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 16/9).
+            tfod.setZoom(2.5, 16.0/9.0);
+        }
     }
 
     public void setCameraPosition(float forward, float left, float height, float heading){
