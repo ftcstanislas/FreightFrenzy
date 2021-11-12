749fb5df51a28b8ef77f5bb10a9ad3a21f8042f2
ftcstanislas <91455801+ftcstanislas@users.noreply.github.com>
2021-11-12 11:15:15 +0100

    removed init error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpV3.java
@@ -82,9 +82,6 @@ public class TeleOpV3 extends OpMode {
 //        spinner.init(hardwareMap, telemetrySpinner);
 //        colorSensor.init(hardwareMap, telemetryColorSensor);
 
-        //Start duck detection
-        location.startDuckDetection();
-
         status.setValue("Initialized");
     }
     
@@ -102,9 +99,6 @@ public class TeleOpV3 extends OpMode {
 //        String test = telemetry.addAction(getStatus);
 //        status.setValue("%s", test);
         lastTime = System.nanoTime();
-
-        //Stop duck detection
-        location.stopDuckDetection();
     }
 
     @Override
