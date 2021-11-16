1f46c8c1f469c195d1361c03dc5f6fb28a6ae17e
FTC Stanislas <ftcscw@gmail.com>
2021-11-16 11:02:33 +0100

    CAMERA works!
    omg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Camera.java
@@ -1,32 +1,4 @@
 package org.firstinspires.ftc.teamcode.Location;
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -55,30 +27,6 @@ import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.RADI
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XZY;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
-
-/**
- * This OpMode illustrates using the Vuforia localizer to determine positioning and orientation of
- * robot on the FTC field using a WEBCAM.  The code is structured as a LinearOpMode
- *
- * NOTE: If you are running on a Phone with a built-in camera, use the ConceptVuforiaFieldNavigation example instead of this one.
- * NOTE: It is possible to switch between multiple WebCams (eg: one for the left side and one for the right).
- *       For a related example of how to do this, see ConceptTensorFlowObjectDetectionSwitchableCameras
- *
- * When images are located, Vuforia is able to determine the position and orientation of the
- * image relative to the camera.  This sample code then combines that information with a
- * knowledge of where the target images are on the field, to determine the location of the camera.
- *
- * Finally, the location of the camera on the robot is used to determine the
- * robot's location and orientation on the field.
- *
- * To learn more about the FTC field coordinate model, see FTC_FieldCoordinateSystemDefinition.pdf in this folder
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- *
- * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
- * is explained below.
- */
 
 public class Camera{
     private static final String VUFORIA_KEY =
@@ -114,7 +62,7 @@ public class Camera{
 
     // Pointer
     Servo pointer = null;
-    private double angle = 0;
+    private double angle = 90;
 
     // Game element detection
     private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
@@ -166,62 +114,14 @@ public class Camera{
         // For convenience, gather together all the trackable objects in one easily-iterable collection */
         allTrackables.addAll(targets);
 
-        /**
-         * In order for localization to work, we need to tell the system where each target is on the field, and
-         * where the phone resides on the robot.  These specifications are in the form of <em>transformation matrices.</em>
-         * Transformation matrices are a central, important concept in the math here involved in localization.
-         * See <a href="https://en.wikipedia.org/wiki/Transformation_matrix">Transformation Matrix</a>
-         * for detailed information. Commonly, you'll encounter transformation matrices as instances
-         * of the {@link OpenGLMatrix} class.
-         *
-         * If you are standing in the Red Alliance Station looking towards the center of the field,
-         *     - The X axis runs from your left to the right. (positive from the center to the right)
-         *     - The Y axis runs from the Red Alliance Station towards the other side of the field
-         *       where the Blue Alliance Station is. (Positive is from the center, towards the BlueAlliance station)
-         *     - The Z axis runs from the floor, upwards towards the ceiling.  (Positive is above the floor)
-         *
-         * Before being transformed, each target image is conceptually located at the origin of the field's
-         *  coordinate system (the center of the field), facing up.
-         */
-
         // Name and locate each trackable object
         identifyTarget(0, "Blue Storage", -halfField, oneAndHalfTile, mmTargetHeight, 90, 0, 90);
         identifyTarget(1, "Blue Alliance Wall", halfTile, halfField, mmTargetHeight, 90, 0, 0);
         identifyTarget(2, "Red Storage", -halfField, -oneAndHalfTile, mmTargetHeight, 90, 0, 90);
         identifyTarget(3, "Red Alliance Wall", halfTile, -halfField, mmTargetHeight, 90, 0, 180);
 
-        /*
-         * Create a transformation matrix describing where the camera is on the robot.
-         *
-         * Info:  The coordinate frame for the robot looks the same as the field.
-         * The robot's "forward" direction is facing out along X axis, with the LEFT side facing out along the Y axis.
-         * Z is UP on the robot.  This equates to a bearing angle of Zero degrees.
-         *
-         * For a WebCam, the default starting orientation of the camera is looking UP (pointing in the Z direction),
-         * with the wide (horizontal) axis of the camera aligned with the X axis, and
-         * the Narrow (vertical) axis of the camera aligned with the Y axis
-         *
-         * But, this example assumes that the camera is actually facing forward out the front of the robot.
-         * So, the "default" camera position requires two rotations to get it oriented correctly.
-         * 1) First it must be rotated +90 degrees around the X axis to get it horizontal (its now facing out the right side of the robot)
-         * 2) Next it must be be rotated +90 degrees (counter-clockwise) around the Z axis to face forward.
-         *
-         * Finally the camera can be translated to its actual mounting position on the robot.
-         *      In this example, it is centered on the robot (left-to-right and front-to-back), and 6 inches above ground level.
-         */
         setCameraPosition(0,0,200,90);//90
-//        final float CAMERA_FORWARD_DISPLACEMENT = 0.0f * mmPerInch;   // eg: Enter the forward distance from the center of the robot to the camera lens
-//        final float CAMERA_VERTICAL_DISPLACEMENT = 6.0f * mmPerInch;   // eg: Camera is 6 Inches above ground
-//        final float CAMERA_LEFT_DISPLACEMENT = 0.0f * mmPerInch;   // eg: Enter the left distance from the center of the robot to the camera lens
-//
-//        OpenGLMatrix cameraLocationOnRobot = OpenGLMatrix
-//                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
-//                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XZY, DEGREES, 90, 90, 0));
-//
-//        /**  Let all the trackable listeners know where the camera is.  */
-//        for (VuforiaTrackable trackable : allTrackables) {
-//            ((VuforiaTrackableDefaultListener) trackable.getListener()).setCameraLocationOnRobot(parameters.cameraName, cameraLocationOnRobot);
-//        }
+
 
         targets.activate();
 
@@ -276,16 +176,21 @@ public class Camera{
 
         // Provide feedback as to where the robot is located (if we know).
         String text = "";
-        if (targetVisible) {
+
+        VectorF translation = null;
+        Orientation rotation= null;
+        if (lastLocation != null) {
             // express position (translation) of robot in inches.
-            VectorF translation = lastLocation.getTranslation();
+            translation = lastLocation.getTranslation();
             text += String.format("Pos (mm) {X, Y} = %.1f, %.1f",
-                    translation.get(0) ,translation.get(1));
+                    translation.get(0), translation.get(1));
 
             // express the rotation of the robot in degrees.
-            Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-            text +=String.format("\nRot (deg) {Heading} = %.0f", rotation.thirdAngle);
+            rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+            text += String.format("\nRot (deg) {Heading} = %.0f", rotation.thirdAngle);
+        }
 
+        if (targetVisible) {
             //blue storage
             double[] location = {-halfField, oneAndHalfTile};
             double[] robotLocationXY = {translation.get(0), translation.get(1)};
@@ -293,7 +198,20 @@ public class Camera{
             double dx = location[0] - robotLocationXY[0];
             double dy = location[1] - robotLocationXY[1];
 //            double angle = Math.atan2(dy,dx)/Math.PI*180 - robotOrientation + 90;
-            angle = 180 - robotOrientation + -Math.atan2(dx,dy)/Math.PI*180;
+            double newAngle = 180 - robotOrientation + -Math.atan2(dx,dy)/Math.PI*180;
+            while (newAngle < 0){
+                newAngle+=360;
+            }
+            while (newAngle >= 360){
+                newAngle-=360;
+            }
+
+            // max speed of servo
+            double difference = newAngle - angle;
+            difference = (difference + 180) % 360 - 180;
+            difference = Math.max(Math.min(difference, 0.05), -0.05);
+            angle += difference;
+
             while (angle < 0){
                 angle+=360;
             }
@@ -309,9 +227,6 @@ public class Camera{
             final double TOTAL_COUNTS_PER_ROUND = 1.27;
             final double OFFSET = 0.045;
 
-            // double pointerAngle = 1/(startCamera-endCamera)*(startCamera-angle);
-            // double pointerAngle = 1-angle/180-0.2;
-//            double pointerPosition = 0.5 * TOTAL_COUNTS_PER_ROUND-TOTAL_COUNTS_PER_ROUND / 360 * angle + OFFSET;
             double pointerPosition = TOTAL_COUNTS_PER_ROUND/360*(180 - angle) + OFFSET;
 
 //            while (pointerPosition < -0.19){
@@ -320,13 +235,13 @@ public class Camera{
 //            while (pointerPosition >= 1.19){
 //                pointerPosition-=2;
 //            }
-            setCameraPosition(0,0,200, (float)(angle));
             pointer.setPosition(pointerPosition);
-//            text += String.format("\nPointer position = %.1f", pointerPosition);
-            text += "\nPointer position = " + pointerPosition;
+            setCameraPosition(0, 0, 200, (float)(angle));
+            text += String.format("\nPointer position = %.1f", pointerPosition);
+//            text += "\nPointer position = %.1f" + pointerPosition;
 
         } else {
-            text +="Visible Target none, heading "+angle;
+            text +="\nVisible Target none\nCamera heading "+angle;
         }
 
         telemetry.setValue(text);
