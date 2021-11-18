451029c8a1d34ac3703657a3717e76a9a963554e
Mr_Stoffer <49318416+mrstoffer@users.noreply.github.com>
2021-11-18 17:07:21 +0100

    Gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Gyro.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.Location;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Gyro {
+
+    private ModernRoboticsI2cGyro gyro;
+    public Telemetry.Item telemetry = null;
+
+    public void init(HardwareMap map, Telemetry.Item telemetryInit) {
+        gyro = map.get(ModernRoboticsI2cGyro.class, "gyro");
+        telemetry = telemetryInit;
+    }
+
+    public double[] getPosition() {
+        double x = gyro.rawX();
+        double y = gyro.rawY();
+        double heading = gyro.getIntegratedZValue();
+
+        double[] position = {x, y, heading};
+        return position;
+    }
+
+    public void calibrate() {
+        telemetry.setValue("Calibrating");
+        gyro.calibrate();
+    }
 }
