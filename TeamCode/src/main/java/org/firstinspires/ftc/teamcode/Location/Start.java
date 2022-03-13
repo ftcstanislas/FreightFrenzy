4adbf66225aab60e554d3826cc701f61d1fbb28e
Mr_Stoffer <49318416+mrstoffer@users.noreply.github.com>
2022-03-13 15:50:56 +0100

    Title
    Description

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Start.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location/Start.java
@@ -1,18 +1,18 @@
 package org.firstinspires.ftc.teamcode.Location;
 
 public class Start {
-    public enum CustomElement {
+    public enum CustomElement { // Determines the level at which the preloaded element will be scored
         LEFT,
         MID,
         RIGHT
     }
 
-    public enum TeamColor {
+    public enum TeamColor { // Determines the directions of robot movement
         RED,
         BLUE
     }
 
-    public enum StartLocation {
+    public enum StartLocation { // Determines what the robot will do to score points
         SPINNER,
         WAREHOUSE
     }
