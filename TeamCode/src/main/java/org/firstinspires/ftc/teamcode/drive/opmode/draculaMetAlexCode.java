8b61ad92c7398214fdc31ee758116b510dccbd36
Stanislas Tech Team <ftcscw@gmail.com>
2023-09-04 16:53:25 +0200

    1 sep werkende drive code + cleaned up AlexSensorUtil.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/draculaMetAlexCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/draculaMetAlexCode.java
@@ -3,33 +3,33 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.robotParts.Drivetrain;
 import org.firstinspires.ftc.teamcode.robotParts.AlexSensorUtil;
+import org.firstinspires.ftc.teamcode.robotParts.DrivetrainAlex;
 
 @TeleOp
 public class draculaMetAlexCode extends LinearOpMode {
-    Drivetrain drivetrain = new Drivetrain();
-    AlexSensorUtil alexsensorutil = new AlexSensorUtil();
+    DrivetrainAlex drivetrain = new DrivetrainAlex();
+    AlexSensorUtil distanceSensor = new AlexSensorUtil();
 
     @Override
     public void runOpMode() throws InterruptedException {
         drivetrain.init(hardwareMap);
-        alexsensorutil.init(hardwareMap);
+        distanceSensor.init(hardwareMap);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // y direction is reversed
-            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_x; // y direction is reversed
+            double x = gamepad1.left_stick_y;
             double rotate = gamepad1.right_stick_x;
             double speedAdder = gamepad1.right_trigger * 0.4;
 
-            Drivetrain.maxSpeed = 0.6 + speedAdder;
+            DrivetrainAlex.maxSpeed = 0.6 + speedAdder;
 
             drivetrain.drive(x, y, rotate);
-            alexsensorutil.workplease(telemetry);
+            distanceSensor.workplease(telemetry);
             telemetry.update();
         }
     }
